84035cae73cc3c790a8e17abdd41ec8c227a9cc3
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-04-28 11:31:06 -0300

    Criação de produto modelo

---
+++ backend/src/main/java/com/produtos/api/produtos_api/modelo/ProdutoModelo.java
@@ -0,0 +1,12 @@
+package com.produtos.api.produtos_api.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProdutoModelo {
+    private Long codigo;
+    private String nome;
+    private String marca;
+}
