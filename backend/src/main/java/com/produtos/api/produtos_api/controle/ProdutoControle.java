90cdec36f3b2c9c6b87b204a9e4651e6dffd2152
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-05-14 11:22:04 -0300

    Rota para listar produtos

--- backend/src/main/java/com/produtos/api/produtos_api/controle/ProdutoControle.java
+++ backend/src/main/java/com/produtos/api/produtos_api/controle/ProdutoControle.java
@@ -1,14 +1,28 @@
 package com.produtos.api.produtos_api.controle;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.api.produtos_api.modelo.ProdutoModelo;
+import com.produtos.api.produtos_api.servico.ProdutoServico;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class ProdutoControle {
 
+    @Autowired
+    ProdutoServico ps;
+
     @GetMapping("/")
     public String rota(){
-        return "A API está funcionando!";
+        return "A API está funcionando!!!";
     }
+    
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return ps.listar();
+    }
+
 }
